b90c417cdffb59947e7b72d8a7088b4ab56b109f
EvanGARAFFA <kingevan2121@gmail.com>
2018-04-30 11:51:24 +0200

    Ajout des textes logins et mot de passe

--- app/src/main/java/sio/evan/gsbandroid/MainActivity.java
+++ app/src/main/java/sio/evan/gsbandroid/MainActivity.java
@@ -1,13 +1,42 @@
+
 package sio.evan.gsbandroid;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import sio.evan.login.Login;
 
-public class MainActivity extends AppCompatActivity {
+public class Main extends Activity
+{
+    private TextView tv;
+    public static final int RESULT_Main = 1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate(Bundle icicle)
+    {
+        super.onCreate(icicle);
+
+        //Appel de la page de Login
+        startActivityForResult(new Intent(Main.this, Login.class), RESULT_Main);
+
+        tv = new TextView(this);
+        setContentView(tv);
+    }
+
+    private void startup(Intent i)
+    {
+        // Récupère l'identifiant
+        int user = i.getIntExtra("userid",-1);
+
+        //Affiche les identifiants de l'utilisateur
+        tv.setText("UserID: "+String.valueOf(user)+" logged in");
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == RESULT_Main && resultCode == RESULT_CANCELED)
+            finish();
+        else
+            startup(data);
     }
 }
